f69b114263538ff44c81b046e19c9ae6330eec5a
micheliknechtel <micheliknechtel@gmail.com>
2018-03-02 09:11:56 +0100

    fix domain extraction and file read

--- Extractor.py
+++ Extractor.py
@@ -13,7 +13,7 @@ class Extractor():
     def __init__(self,pathInput, pathOutput):
         self.pathInput  = pathInput
         self.pathOutput = pathOutput
-
+    
     def extractingFiles(self):
         r = Repository()
         c = CodecDetection()
@@ -24,33 +24,57 @@ class Extractor():
         array = []
         for match in finditer(pattern, line):
             array.append(match.group().strip('"'))
+        if  not array:
+            array.append(" ")
         return array
-            
+
     def getDomains(self, urls):
         domains = []
         for i in range(len(urls)):
-            domains.append(tldextract.extract(urls[i]).domain)
+            subdomain = tldextract.extract(urls[i]).subdomain
+            domain    = tldextract.extract(urls[i]).domain[0:3]
+            suffix    = tldextract.extract(urls[i]).suffix
+            
+            if not subdomain:
+                domains.append(domain + "." + suffix)
+            else:
+                domains.append(subdomain+ "." +domain + "." + suffix)
         return domains
 
     def parse(self, fileName, codec):
         location  = self.pathInput + fileName
         fileContent = ""
         receivers = []
-        
+        sender = {}
         with open(location, 'rt', encoding=codec) as file:
             lines = file.read().splitlines()
             for i in range(len(lines)):
-                line = lines[i]
+                auxLine = lines[i]
+                line = lines[i].strip("=")
                 if Definition.FROM in line:
-                    sender = {"name":str(self.regex(line, Pattern.Name)), "email":str(self.regex(line, Pattern.Email))}
+                    sender = {"name":self.regex(line, Pattern.Name)[0], "email":self.regex(line, Pattern.Email)[0]}
                 if Definition.TO  in line:
-                    receivers.append({"name":str(self.regex(line, Pattern.Name)),"email":str(self.regex(line, Pattern.Email))})
-                fileContent = fileContent + lines[i]
-            urls = (self.regex(fileContent, Pattern.Url))
-            email = (self.regex(fileContent, Pattern.Email))
+                    receivers.append({"name":self.regex(line, Pattern.Name)[0],"email":self.regex(line, Pattern.Email)[0]})
+                fileContent = fileContent + line
+
         file.close()
+        urls = (self.regex(fileContent, Pattern.Url))
+        email = (self.regex(fileContent, Pattern.Email))
         domains = self.getDomains(urls)
+        
         data = {"email_id":int(fileName), "email":email, "domains":domains, "sender":sender, "receivers":receivers}
         SerialiseData(data).writeIntoFile(self.pathOutput)
 
+        for i in range(len(urls)):
+            print(domains[i])
+        
+        print("------------------------")
+        print(fileName)
+        print("------------------------")
 
+        if fileName == "1000":
+            SerialiseData(data).writeIntoFile(self.pathOutput)
+            for i in range(len(urls)):
+                print(urls[i])
+                print(domains[i])
+            sys.exit()
--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from Extractor import Extractor
 from Repository import Repository
 
 
-pathInput  = "/Users/micheliknechtel/Documents/xCode/ivizone/Email-Extraction-Project/email/"
+pathInput  = "/Users/micheliknechtel/Documents/xCode/ivizone/Email-Extraction-Project/email2/"
 pathOutput = "/Users/micheliknechtel/Documents/xCode/ivizone/Email-Extraction-Project/json/"
 
 Repository().checkRepositoryExist(pathInput)
 Repository().checkRepositoryExist(pathOutput)
+
+print(pathInput[0:3])
 Extractor(pathInput, pathOutput).extractingFiles()
 
 
